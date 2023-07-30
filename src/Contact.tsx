434c6cff7fedb28b4ee4a6515c53c1bf08123da9
nholdsworth94 <nick.holdsworth@me.com>
2023-07-30 21:00:38 +0100

    upates to redirect emailjs integration in progress updated email links

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,8 +1,48 @@
 import { Button, Grid, TextField } from "@mui/material";
 import "./Contact.scss";
 import SendIcon from "@mui/icons-material/Send";
+import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  useEffect(() => emailjs.init("YOUR-PUBLIC-KEY-HERE"), []);
+
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
+
+  const regexp = new RegExp(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+
+  const formIsValid = () => {
+    return !!name && ((!!email && regexp.test(email)) || !!phone) && !!query;
+  };
+
+  const submit = () => {
+    emailjs
+      .send(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        {
+          name,
+          email,
+          phone,
+          query,
+        },
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result: any) => {
+          console.log(result.text);
+        },
+        (error: any) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact">
       <h1>Contact Me</h1>
@@ -10,7 +50,9 @@ const Contact = () => {
       <p>
         Please feel free to contact me directly by calling me on{" "}
         <a href="tel:07968814135">07969071786</a> or by emailing me at{" "}
-        <a href="mailto:andy@solihullpchelp.co.uk">andy@solihullpchelp.co.uk</a>
+        <a href="mailto:solihullpchelp@outlook.com">
+          solihullpchelp@outlook.com
+        </a>
       </p>
 
       <h1>Request a Callback</h1>
@@ -23,6 +65,8 @@ const Contact = () => {
               label="Name"
               variant="outlined"
               required
+              value={name}
+              onChange={(v) => setName(v.target.value)}
             />
           </Grid>
           <Grid xs={4} item>
@@ -31,6 +75,8 @@ const Contact = () => {
               label="Email"
               variant="outlined"
               required
+              value={email}
+              onChange={(v) => setEmail(v.target.value)}
             />
           </Grid>
           <Grid xs={4} item>
@@ -39,6 +85,8 @@ const Contact = () => {
               label="Phone Number"
               variant="outlined"
               required
+              value={phone}
+              onChange={(v) => setPhone(v.target.value)}
             />
           </Grid>
           <Grid xs={12} item>
@@ -49,10 +97,17 @@ const Contact = () => {
               multiline
               rows={4}
               required
+              value={query}
+              onChange={(v) => setQuery(v.target.value)}
             />
           </Grid>
           <Grid xs={12} item>
-            <Button variant="contained" endIcon={<SendIcon />}>
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={submit}
+              disabled={!formIsValid()}
+            >
               Send
             </Button>
           </Grid>
--- src/Core/Menu.tsx
+++ src/Core/Menu.tsx
@@ -1,28 +1,27 @@
-import StyledLink from '../Shared/StyledLink';
-import './Menu.scss';
+import StyledLink from "../Shared/StyledLink";
+import "./Menu.scss";
 
 const Menu = () => {
-
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <StyledLink to='/'>Home</StyledLink>
-                </li>
-                {/* <li>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <StyledLink to="/">Home</StyledLink>
+        </li>
+        {/* <li>
                     <StyledLink to='/about'>About</StyledLink>
                 </li> */}
-                <li>
-                    <StyledLink to='/contact'>Contact</StyledLink>
-                </li>
-                <li>
-                    <a href='tel:07968814135' className='phone'>
-                        Call: 079 688 141 35
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+        <li>
+          <StyledLink to="/contact">Contact</StyledLink>
+        </li>
+        <li>
+          <a href="tel:07968814135" className="phone">
+            Call: 079 688 141 35
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Menu;
